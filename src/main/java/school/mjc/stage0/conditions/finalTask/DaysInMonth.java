1b526df48a18262a7b36d8922dcbb53b0bf94321
shenanigami <38479116+shenanigami@users.noreply.github.com>
2023-09-02 21:48:52 +0600

    First batch of changes

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,30 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
+
+    public boolean isLeapYear(int year) {
+
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+            return true;
+        else
+            return false;
+    }
     public void printDays(int year, int month) {
 
+        if (month > 12 || month < 1 || year < 0) {
+            System.out.println("invalid date");
+            return;
+        }
+
+        if (month == 2) {
+            if (isLeapYear(year))
+                System.out.println(29);
+            else
+                System.out.println(28);
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            System.out.println(30);
+        } else {
+            System.out.println(31);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        if (monthNumber > 12 || monthNumber < 1) {
+            System.out.println("Wrong month number");
+        } else if (monthNumber == 12 || monthNumber < 3) {
+            System.out.println("Winter");
+        } else if (monthNumber >= 9) {
+            System.out.println("Autumn");
+        } else if (monthNumber >= 6) {
+            System.out.println("Summer");
+        } else {
+            System.out.println("Spring");
+        }
     }
 }
